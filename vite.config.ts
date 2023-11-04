ce76c54705d686d516df8814deee4ace54eca35a
AlexDyatlov <aleks78.gooogle@gmail.com>
2023-11-04 15:51:43 +0300

    Added Docker and docker-compose

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,14 @@ import react from '@vitejs/plugin-react';
 import eslintPlugin from 'vite-plugin-eslint';
 
 export default defineConfig({
+  server: {
+    host: true,
+    port: Number(process.env.VITE_PORT) || 8080,
+    hmr: true
+  },
+  preview: {
+    port: Number(process.env.VITE_PORT) || 8080
+  },
   plugins: [
     react(),
     eslintPlugin({
